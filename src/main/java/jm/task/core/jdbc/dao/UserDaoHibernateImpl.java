8679c622ace40926de82bd5ba077c90889d76a74
Salavat <reinofgrein@gmail.com>
2022-07-20 12:50:25 +0300

    Finish Commit.

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,8 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
 
 import java.util.List;
 
@@ -12,31 +14,91 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        try(Session session = Util.getConnection().getCurrentSession()) {
+            try{
+              session.beginTransaction();
+              String sql = "CREATE TABLE IF NOT EXISTS users " +
+                      "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                      "name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, " +
+                      "age TINYINT NOT NULL);";
+              session.createSQLQuery(sql).executeUpdate();
+              session.getTransaction().commit();
 
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try(Session session = Util.getConnection().getCurrentSession()){
+            try{
+                session.beginTransaction();
+                String sql = "drop table if exists users";
+                session.createSQLQuery(sql).executeUpdate();
+                session.getTransaction().commit();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try(Session session = Util.getConnection().getCurrentSession()){
+            try{
+                session.beginTransaction();
+                User user = new User(name, lastName, age);
+                session.save(user);
+                System.out.println(user.getName() + " был добавлен в базу данных.");
+                session.getTransaction().commit();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try(Session session = Util.getConnection().getCurrentSession()){
+            try{
+                session.beginTransaction();
+                User user = session.get(User.class, id);
+                session.remove(user);
+                session.getTransaction().commit();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> list = null;
+        try(Session session = Util.getConnection().getCurrentSession()){
+            try{
+                session.beginTransaction();
+                list = session.createQuery("select i from User i").getResultList();
+                session.getTransaction().commit();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try(Session session = Util.getConnection().getCurrentSession()){
+            try{
+                session.beginTransaction();
+                String sql = "delete from users";
+                session.createSQLQuery(sql).executeUpdate();
+                session.getTransaction().commit();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static SessionFactory sessionFactory;
+    public static SessionFactory getConnection(){
+        if (sessionFactory == null){
+            try {
+                /*
+                    Благодаря SessionFactory, а точнее тому, что я в конфигах настроил путь до БД
+                    в теге <SessionFactory> - мне не приходится больше нихуя делать кроме как указать конф файл.
+                    Его кстати тоже можно не указывать если он назван по умолчанию (так, как назван сейчас)
+                 */
+                sessionFactory =  new Configuration()
+                        .configure("hibernate.cfg.xml")
+                        .addAnnotatedClass(User.class) //Тут указываем класс, который имеет специальные аннотации
+                        .buildSessionFactory();
+            } catch (HibernateException e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
 }
